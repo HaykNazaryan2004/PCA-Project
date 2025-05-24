c8a25534698cce5bd2482c9d0180e20667ffa78a
HaykNazaryan2004 <haykon41@gmail.com>
2025-05-24 11:44:07 +0400

    Update PCA1.R

--- PCA1.R
+++ PCA1.R
@@ -1,7 +1,8 @@
-# Երկու ձևով տվյալների վրա կիռարել ենք ներոնային ցանց, որպեսզի հասկանանք PCA-ի ազդեցությունը ուսուցման ժամանակի և ճշտության վրա։ 
+# Երկու ձևով տվյալների վրա կիռարել ենք ներոնային ցանց(Fashion MNIST տվյալների բազայի վրա), որպեսզի հասկանանք PCA-ի ազդեցությունը ուսուցման ժամանակի և ճշտության վրա։ 
 # Առաջին կոդում նվազեցրել ենք չափողականությունը  PCA-ի միջոցով հետո կիրառել նեյրոնային ցանց։ Իսկ առաջին կոդում միանգամից կիրառել ենք նեյրոնային ցանց։
 # Սիդը նվիրված է ԵՊՀ-ի 106 ամյակին
-
+# Fashion MNIST տվյալների բազան պատկերների դասակարգման խնդիրների համար օգտագործվող հանրաճանաչ չափանիշային տվյալների բազա է:
+# Այն նախատեսված է որպես MNIST-ի (ձեռագիր թվանշաններ) ավելի իրատեսական այլընտրանք, որը պարունակում է հագուստի մոխրագույն երանգների պատկերներ:
 
 
 # Նեռբեռնել ենք անրաժեշտ գրադարանները 
@@ -21,40 +22,40 @@ set.seed(106)
 
 # Fashion mnist տվյալների ներբեռնում
 fashion_mnist <- dataset_fashion_mnist()
-c(train_images, train_labels) %<-% fashion_mnist$train
-c(test_images, test_labels) %<-% fashion_mnist$test
+c(train_images, train_labels) %<-% fashion_mnist$train # Առանձնացնում է ուսուցանվող տվյալները նկար-իրեն համապատասխան դաս
+c(test_images, test_labels) %<-% fashion_mnist$test # նույն գործողությունը՝ այստեղ
 
 # Նկարները դարձնենք միաչափ վեկտորներ (28x28 դարձնենք 784x1)
-train_images <- array_reshape(train_images, c(nrow(train_images), 784)) / 255
-test_images <- array_reshape(test_images, c(nrow(test_images), 784)) / 255
+train_images <- array_reshape(train_images, c(nrow(train_images), 784)) / 255 # այս տողը դարձնում է մեկ չափանի վեկտոր մեր նկարներև և քանի որ պիքսելները արժեք են ընդոuնում (0,255) բաժանում ենք 255-ի,որպեսզի ընդունի (0,1)  արժեքներ 
+test_images <- array_reshape(test_images, c(nrow(test_images), 784)) / 255 # նույն գործողությունը կատարել ենք թեստավորվող տվյալների
 
 #  PCA-ի կիրառում
-pca_model <- prcomp(train_images, center = TRUE, scale. = TRUE)
+pca_model <- prcomp(train_images, center = TRUE, scale. = TRUE) #Էստեղ տվյալներից հանել ենք միջինը և բաժանել ենք ստանդարտ շեղման վրա,որը ընդունված պրակտիկա է PCA կիրառելուց առաջ
 
 # Գտնենք այն կոպոնենտների քանակը(գծային տարածության չափողականությունը) որոնք պահանում են 95% վարիացիա
-explained_variance <- cumsum(pca_model$sdev^2 / sum(pca_model$sdev^2))
-num_components <- which(explained_variance >= 0.95)[1]
+explained_variance <- cumsum(pca_model$sdev^2 / sum(pca_model$sdev^2)) # այս տողը արտածում է թե առաջին n կոմպոնենտները քանի % վարիացիա են բացատրում
+num_components <- which(explained_variance >= 0.95)[1] # Սա գտնում է առաջին կոմպոնենտի ինդեքսը, որտեղ գումարային դիսպերսիան ≥ 95% է։
 cat("կոմպոնենտների քանակը, որոնք պարունակում են  95% վարիացիա:", num_components, "\n")
 
 # բաժանենք ուսուցանվող և թեստավորվող տվյաներ(train-ի և test-ի բաժանում)
-train_pca <- predict(pca_model, train_images)[, 1:num_components]
-test_pca <- predict(pca_model, test_images)[, 1:num_components]
+train_pca <- predict(pca_model, train_images)[, 1:num_components] # կիրառում ենք PCA ուսուցանվող տվյալների վրա և պրոյեկտում տվյալները առաջին num_components-ով ենթատարածության վրա։
+test_pca <- predict(pca_model, test_images)[, 1:num_components]   # կիրառում ենք PCA թեստավորվող տվյալների վրա և պրոյեկտում տվյալները առաջին num_components-ով ենթատարածության վրա։
 
 # կատերգորիկ փոփոխականները դարձրել ենք թվային
-train_labels_cat <- to_categorical(train_labels, 10)
+train_labels_cat <- to_categorical(train_labels, 10) 
 test_labels_cat <- to_categorical(test_labels, 10)
 
 # կառուցենք նեյրոնային ցանց
 model_raw <- keras_model_sequential() %>%
-  layer_dense(units = 128, activation = 'relu', input_shape = c(784)) %>%
-  layer_dropout(0.3) %>%
+  layer_dense(units = 128, activation = 'relu', input_shape = c(784)) %>% # layer 128 նեյրոնով և relu=max(0, x) activation function-ով(որը դարձնում է մոդելին ոչ գծային)
+  layer_dropout(0.3) %>% # նեյրոնների 30%-ը անջատում է,օգնում է վերացնել overfitting-ի դեպքը
   layer_dense(units = 64, activation = 'relu') %>%
   layer_dense(units = 10, activation = 'softmax')
 
 model %>% compile(
-  optimizer = 'adam',
-  loss = 'categorical_crossentropy',
-  metrics = 'accuracy'
+  optimizer = 'adam', # կարգարովորում է learning rate-ը ուսուցման ընթացքում,որպեսզի ավելի արագ զուգամիտի
+  loss = 'categorical_crossentropy', # Այն չափում է, թե որքանով է կանխատեսված հավանականությունը հեռու իրականից։
+  metrics = 'accuracy' # Ճիշտ կանխատեսումների տոկոսը
 )
 
 # չափենք ուսուցման ժամանակը
@@ -62,13 +63,12 @@ start_time_pca <- Sys.time()
 
 # ուսուցանել մոդելը
 history <- model %>% fit(
-  x = train_pca,
-  y = train_labels_cat,
-  epochs = 20,
-  batch_size = 128,
-  validation_split = 0.2,
+  x = train_pca, # Սրանք ուսուցման մուտքային feature-ներն են
+  y = train_labels_cat, # Սրանք թիրախային label-ներն են՝ one-hot encoded տեսքով
+  epochs = 20, # մոդելը անցնում է ամբողջ տվյալների միջով 20 անգամ
+  batch_size = 128, # Ուսուցման տվյալները կբաժանվեն 128 նմուշներից բաղկացած խմբերի(375 խումբ),Մոդելը կշիռները թարմացնում է մեկ խմբի համար, այլ ոչ թե յուրաքանչյուր նմուշի համար (սա արագացնում է ուսուցումը)
+  validation_split = 0.2, # Ուսուցման տվյալների 20%-ը կօգտագործվի վալիդացիայի համար (ստուգելու համար, թե որքան լավ է մոդելը ընդհանրացնում)։Մոդելը չի ​​ուսուցանվում այս հատվածի վրա, բայց յուրաքանչյուր epoch-ից հետո հաղորդում է դրա ճշգրտության և կորստի մասին։
   verbose = 2
-
 )
 
 # գնահատենք ճշտությունը տեստ(test)- դատայի վրա
